be9b539af6e19480824b463ace621f534c4c2aee
Michael <m.nwuju@alustudent.com>
2024-09-30 11:40:27 -0700

    pushing my assignment

---
+++ create_environment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p submission_reminder_app
+
+mkdir -p submission_reminder_app/app
+
+mkdir -p submission_reminder_app/modules
+
+mkdir -p submission_reminder_app/config
+
+mkdir -p submission_reminder_app/assets
+
+touch submission_reminder_app/modules/functions.sh
+
+touch submission_reminder_app/app/reminder.sh
+
+touch submission_reminder_app/assets/submission.txt
+
+touch submission_reminder_app/config/config.env
---
+++ submission_reminder_app/startup.sh
@@ -0,0 +1,4 @@
+!#/bin/bash
+echo "Starting Reminder App"
+
+./app/reminder.sh
