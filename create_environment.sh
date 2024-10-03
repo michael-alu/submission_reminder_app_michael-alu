ed957c55c0b2430b4a6973e57e2ffaf9d5ce9c05
Michael <m.nwuju@alustudent.com>
2024-10-03 01:48:07 -0700

    improve create environment flow

--- create_environment.sh
+++ create_environment.sh
@@ -21,3 +21,29 @@ touch "$script_dir/submission_reminder_app/app/reminder.sh"
 touch "$script_dir/submission_reminder_app/assets/submissions.txt"
 
 touch "$script_dir/submission_reminder_app/config/config.env"
+
+cp "$script_dir/submissions.txt" "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "Michael, Shell Navigation, submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "Kolapo, Shell Navigation, not submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "Ibrahim, Shell Navigation, not submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "Hero, Shell Navigation, submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+echo "Bruce, Shell Navigation, submitted" >> "$script_dir/submission_reminder_app/assets/submissions.txt"
+
+cat "$script_dir/reminder.sh" >> "$script_dir/submission_reminder_app/app/reminder.sh"
+
+cat "$script_dir/functions.sh" >> "$script_dir/submission_reminder_app/modules/functions.sh"
+
+cat "$script_dir/config.env" >> "$script_dir/submission_reminder_app/config/config.env"
+
+chmod u+x "$script_dir/submission_reminder_app/app/reminder.sh"
+
+chmod u+x "$script_dir/submission_reminder_app/modules/functions.sh"
+
+chmod u+x "$script_dir/submission_reminder_app/config/config.env"
+
+echo "Done setting up the Environment"
